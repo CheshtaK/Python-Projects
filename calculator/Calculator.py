c08dcbe9f85611e864e7c3fd8492f245c611d1a7
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-04-10 21:28:10 +0530

    importing calculator.py and using lists

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -24,13 +24,13 @@ def div(num1, num2):
 
 def runOperation(operation, num1, num2):
     """Determines the operation to run"""
-    if operation == 1:
+    if operation == 1 or operation == '+':
         print(add(num1, num2))
-    elif operation == 2:
+    elif operation == 2 or operation == '-':
         print(sub(num1, num2))
-    elif operation == 3:
+    elif operation == 3 or operation == '*':
         print(mul(num1, num2))
-    elif operation == 4:
+    elif operation == 4 or operation == '/':
         print(div(num1, num2))
     else:
         print("I don't understand")
@@ -38,30 +38,28 @@ def runOperation(operation, num1, num2):
 
 def main():
 
-    userContinue = True
-    while userContinue:
-        validInput = False
+    validInput = False
 
-        while not validInput:
-            try:
-                num1 = int(input("Enter first number"))
-                num2 = int(input("Enter second number"))
-                operation = int(input("What do you want to do? 1.Add, 2.Subtract, 3.Multiply, 4.Divide. "
-                                      "Enter number: "))
-                validInput = True
-            except ValueError:
-                print("Invalid input. Try again")
-            except:
-                print("Unknown error")
-
+    while not validInput:
+        try:
+            num1 = int(input("Enter first number"))
+            num2 = int(input("Enter second number"))
+            operation = int(input("What do you want to do? 1.Add, 2.Subtract, 3.Multiply, 4.Divide. Enter number: "))
+            validInput = True
+        except ValueError:
+            print("Invalid input. Try again")
+        except:
+            print("Unknown error")
         runOperation(operation, num1, num2)
-
-        userYN = input("Would you like to run another calculation? ('y' for yes or any other value to exit)")
-        if(userYN != 'y'):
-            userContinue = False
-            break
-        else:
-            continue
-
-
-main()
+# This is a fancy line of code that will
+# only run the main function if we directly execute this
+# file. Think of the __name__ variable as an indicator
+# assigned by python for the purpose of determining
+# if the "main" function should be run. Adding this
+# line will enable use to import our calculator
+# "module" so we can reuse its functionality.
+# If you do not totally understand why this
+# works DO NOT WORRY. Just know what it does
+# and WHY we are using it.
+if __name__ == "__main__":
+    main()
