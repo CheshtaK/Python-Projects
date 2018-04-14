96735ceb4ec1890fed4d6bae59f0c10d4d9ead51
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-04-14 19:40:12 +0530

    reading and writing to a file

---
+++ file basics/ReadWrite.py
@@ -0,0 +1,16 @@
+filename = "file.txt"
+
+## Writing
+
+file = open(filename, 'w')
+
+for i in range(1, 11):
+    file.write("This is line %i \n" % i)
+file.close()
+
+
+file = open(filename, 'r')
+
+for line in file:
+    print(line, end = '')
+file.close()   
